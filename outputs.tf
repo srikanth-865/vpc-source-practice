6ac5233c85f83588e0fb42ccf1293482e13e1874
unknown <srikanthram87@gmail.com>
2026-08-11 07:14:11 +0530

    oil&masala

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@
 output "vpc_id"{
     value = aws_vpc.main.id
 }
+
+output "public_subnet_ids"{
+    value = aws_subnet.public.value
+}
+
+output "private_subnet_ids"{
+    value = aws_subnet.public.value
+}
+
+output "database_subnet_ids"{
+    value = aws_subnet.public.value
+}
